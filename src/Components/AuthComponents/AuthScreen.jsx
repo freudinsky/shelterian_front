14bd94812ea466dba72886bc85653ac73bb451ff
freudinsky <timo.fuchs94@icloud.com>
2023-11-07 01:25:40 +0100

    ..

--- src/Components/AuthComponents/AuthScreen.jsx
+++ src/Components/AuthComponents/AuthScreen.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { useAuth } from "../../Context/AuthProv";
 
 function AuthScreen() {
-	const { loggedIn } = useAuth();
-
 	return (
 		<>
-			{ loggedIn ?
-				<div className="auth-screen">
-					<Outlet />
-				</div> : ""
-			}
+			<div className="auth-screen">
+				<Outlet />
+			</div>
 		</>
 	);
 }
