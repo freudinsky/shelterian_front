a274d4a1c5d923aa0768de2490236a1a1e82def5
freudinsky <me@timo-fuchs.com>
2023-11-12 22:17:32 +0100

    bugfixes

--- 0_ToDos.md
+++ 0_ToDos.md
@@ -2,18 +2,18 @@
 
 - Animal edit page
 - Account edit page
+- Implementation Adoption-Terms (Tooltip/Popover/Modal)
 - Change backend error messages to german (for toastify)
-- Detail page -> gallery & details in row instead of col
-- Verification document upload on SignUp (animals only visible if verified, maybe option to upload later?) -> additional DB field in Shelter schema for verified
+- Verification document upload on SignUp (maybe option to upload later?)
 - Reserve/Hold Badge + DB field in Dogs+Cats schema
 - Dark Mode
 - Responsiveness
 - Redesign
 - Context -> Redux for state management?
-- Password Validation -> strong PWs!
-- Implementation Adoption-Terms
 
 ----------------------------------------------------------------------------------
 
 ## Done
 - Email validation & double opt-in (GMail API, Google Workspace -> add domain in Google Workspace)
+- Detail page -> gallery & details in row instead of col
+- Password Validation -> strong PWs!
--- src/Components/AuthComponents/SignUp.jsx
+++ src/Components/AuthComponents/SignUp.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
-import { Button } from "@nextui-org/react";
+import { Button, Tooltip } from "@nextui-org/react";
 
 function SignUp() {
 	const [name, setName] = useState("");
@@ -17,9 +17,19 @@ function SignUp() {
 	const [password, setPassword] = useState("");
 	const [confirmPwd, setConfirmPwd] = useState("");
 	const [pwdSame, setPwdSame] = useState(true);
+	const [pwdStrong, setPwdStrong] = useState();
 	const [loading, setLoad] = useState(false);
 	const nav = useNavigate();
 
+	const strongPwdRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+
+	const strongPwd = () => {
+		if (strongPwdRegex.test(password)) {
+			setPwdStrong(true);
+		} else {
+			setPwdStrong(false);
+		}
+	};
 	const checkPwd = () => {
 		if (confirmPwd) {
 			if (password === confirmPwd) {
@@ -32,6 +42,7 @@ function SignUp() {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		checkPwd()
 		setLoad(true);
 
 		if (
@@ -146,6 +157,7 @@ function SignUp() {
 					onChange={(e) => setPhone(e.target.value)}
 				/>
 				<input
+					autocomplete="email"
 					className="my-1 border-solid border-1 border-slate-300 rounded-3xl h-10 indent-5"
 					type="email"
 					placeholder="E-Mail"
@@ -154,14 +166,56 @@ function SignUp() {
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 				<input
-					className="my-1 border-solid border-1 border-slate-300 rounded-3xl h-10 indent-5"
+					className={
+						pwdStrong
+							? "my-1 border-solid border-1 border-slate-300 rounded-3xl h-10 indent-5"
+							: "my-1 border-solid border-1 border-red-500 rounded-3xl h-10 indent-5"
+					}
 					type="password"
 					placeholder="Passwort"
+					autocomplete="new-password"
 					value={password}
+					name="password"
 					required
 					onChange={(e) => setPassword(e.target.value)}
+					onBlur={strongPwd}
 				/>
+				{!pwdStrong ? (
+					<Tooltip
+						showArrow={true}
+						content={
+							<div className="px-4 py-2  text-white">
+								<p>Das Passwort muss:</p>
+								<ul className="list-disc font-semibold pl-6">
+									<li>mind. 1 Kleinbuchstaben</li>
+									<li>mind. 1 Großbuchstaben</li>
+									<li>mind. 1 Sonderzeichen</li>
+									<li>mind. 1 Zahl</li>
+									<li>mind. 8 Zeichen</li>
+								</ul>
+								<p>enthalten.</p>
+							</div>
+						}
+						className="bg-rose-900"
+						placement="left"
+					>
+						<div className="flex items-center gap-1 px-2">
+							<img
+								width="17"
+								height="17"
+								src="https://img.icons8.com/ios-glyphs/30/ef4444/info--v1.png"
+								alt="info--v1"
+							/>
+							<label className="text-sm text-red-500" for="password">
+								Schwaches Passwort.{" "}
+							</label>
+						</div>
+					</Tooltip>
+				) : (
+					""
+				)}
 				<input
+					autoComplete=""
 					className={
 						pwdSame
 							? "my-1 border-solid border-1 border-slate-300 rounded-3xl h-10 indent-5"
@@ -169,11 +223,22 @@ function SignUp() {
 					}
 					type="password"
 					placeholder="Passwort wiederholen"
+					autocomplete="new-password"
 					value={confirmPwd}
 					onChange={(e) => setConfirmPwd(e.target.value)}
-					onBlur={checkPwd}
 					required
 				/>
+				{!pwdSame? <div className="flex items-center gap-1 mb-2 px-2">
+							<img
+								width="17"
+								height="17"
+								src="https://img.icons8.com/ios-glyphs/30/ef4444/info--v1.png"
+								alt="info--v1"
+							/>
+							<label className="text-sm text-red-500" for="password">
+								Passwörter müssen übereinstimmen.
+							</label>
+						</div> :""}
 				<Button
 					className="mt-1 bg-rose-800 text-white font-semibold px-7 py-1 rounded-3xl h-10"
 					onClick={handleSubmit}
