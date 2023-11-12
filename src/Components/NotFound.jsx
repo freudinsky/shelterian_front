62af02e0abd44092378e02ce63ba82cb1e871d41
freudinsky <me@timo-fuchs.com>
2023-11-12 19:28:02 +0100

    validation

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -8,13 +8,18 @@ function NotFound() {
   return (
 		<div className="w-full mx-auto max-w-screen-md h-96 flex flex-col justify-center items-center">
 			<div>
-			  <h1 className="m-0 p-0 text-8xl text-neutral-600 text-center">404</h1>
-  			<h2 className="m-0 p-0 text-5xl text-neutral-600 text-center my-6">
-				{" "}
-				Da hat sich wohl jemand verirrt?
-			</h2>
+				<h1 className="m-0 p-0 text-8xl text-neutral-600 text-center">404</h1>
+				<h2 className="m-0 p-0 text-5xl text-neutral-600 text-center my-6">
+					{" "}
+					Da hat sich wohl jemand verirrt?
+				</h2>
 			</div>
-<Button className="rounded-full text-lg font-semibold mt-6" color="danger" onClick={()=> nav('/')} >Zurück zu Home</Button>
+			<Button
+				className="bg-rose-800 rounded-full text-lg font-semibold mt-6"
+				onClick={() => nav("/")}
+			>
+				Zurück zu Home
+			</Button>
 		</div>
 	);
 }
